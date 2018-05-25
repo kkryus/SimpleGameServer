7cad72c37cb5d4c42a8d104b505662d01ce8dd68
kkryus <kamilkryus@gmail.com>
2018-05-25 13:27:56 +0200

    page creates games dynamically

--- injectGame.js
+++ injectGame.js
@@ -1,10 +1,5 @@
 ﻿function injectGames() 
 {
-	var amountOfBlocks = 16;
-	for(i = 0;i<amountOfBlocks;i++)
-	{
-		document.getElementsByClassName("cell"+ i)[0].style.display= 'none';
-	}
 	var amountOfGames = 0;
 	 $.ajax({
         url: "phpScripts/getAmountOfGames.php",
@@ -55,27 +50,57 @@
 		});	
 		authors = authors.split(",");
 		gameObjects[i] = {name:gameNames[i], author1:authors[0], author2:authors[1]};
-	}
+	}			
 			
+	
+	var container = document.getElementsByClassName("container")[0];
 	
 	for(i = 0;i< amountOfGames;i++)
 	{
-		document.getElementsByClassName("cell"+ i)[0].style.display= 'block';
-		document.getElementsByClassName("gameName")[i].innerHTML = "<center>" + gameNames[i] + "</center>";
-		document.getElementsByClassName("card-img-top")[i].src = "games/" + gameNames[i] + "/pic.jpg";
-		document.getElementsByClassName("card-img-top")[i].width = 365;
-		document.getElementsByClassName("card-img-top")[i].height = 365;
-		document.getElementsByClassName("link")[i].href = "games/" + gameNames[i] +"/index.html";
-		if(gameObjects[i].author2 != undefined)
-		{
-			//document.getElementsByClassName("card-body")[i].innerHTML = "<p class=\"card-text\"><nobr>"+ gameObjects[i].author1 + " </nobr></br><nobr>" + gameObjects[i].author2 + "</nobr></p> ";
-			document.getElementsByClassName("card-text")[i].innerHTML = "<nobr>"+ gameObjects[i].author1 + " </nobr></br><nobr>" + gameObjects[i].author2 + "</nobr>";
-		}
-		else
-		{
-			document.getElementsByClassName("card-text")[i].innerHTML = "<nobr>"+ gameObjects[i].author1+ "</nobr>";
-			//document.getElementsByClassName("card-body")[i].innerHTML = "<p class=\"card-text\"><nobr>"+ gameObjects[i].author1+ "</nobr></p> ";
-		}
+		
+		var divCell = document.createElement("div");
+		container.appendChild(divCell);
+			var divColMd4 = document.createElement("div");
+			divColMd4.classList.add("col-md-4");
+		divCell.appendChild(divColMd4);
+				var divCardMb4BoxShadow = document.createElement("div");
+				divCardMb4BoxShadow.classList.add("card");
+				divCardMb4BoxShadow.classList.add("mb-4");
+				divCardMb4BoxShadow.classList.add("box-shadow");
+			divColMd4.appendChild(divCardMb4BoxShadow);
+					var link = document.createElement("a");
+					link.href = "games/" + gameNames[i] + "/index.html";
+						var gameName = document.createElement("p");
+							var gameNameTextNode = document.createTextNode(gameNames[i]);
+						gameName.appendChild(gameNameTextNode);
+					link.appendChild(gameName);
+						var img = document.createElement("img");
+						img.classList.add("card-img-top");
+						img.width = 365;
+						img.height = 365;
+						img.style.border='1px solid #000000';
+						img.src = "games/" + gameNames[i] + "/pic.jpg";
+					link.appendChild(img);
+				divCardMb4BoxShadow.appendChild(link);
+					var cardBodyDiv = document.createElement("div");
+					cardBodyDiv.classList.add("card-body");
+						var authorsElement = document.createElement("p");
+							var noBrElementOne = document.createElement("nobr");
+							var authorTextNode = document.createTextNode(gameObjects[i].author1);
+							noBrElementOne.appendChild(authorTextNode);
+						authorsElement.appendChild(noBrElementOne);
+							if(gameObjects[i].author2 != undefined)
+							{
+								var brElement = document.createElement("br");
+								var noBrElementTwo = document.createElement("nobr");
+								var secondAuthorTextNode = document.createTextNode(gameObjects[i].author2);
+								noBrElementTwo.appendChild(secondAuthorTextNode);
+								authorsElement.appendChild(brElement);
+								authorsElement.appendChild(noBrElementTwo);
+							}
+						
+					cardBodyDiv.appendChild(authorsElement);
+				divCardMb4BoxShadow.appendChild(cardBodyDiv);		
 	}
 					
 }
